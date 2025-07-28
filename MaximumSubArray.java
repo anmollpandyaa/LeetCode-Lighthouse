4adef3e77f5eb70924ca80c574e7b9e6e4864557
Anmol Pandya <150453488+anmollpandyaa@users.noreply.github.com>
2025-07-28 10:41:26 +0530

    Create MaximumSubArray.java

---
+++ MaximumSubArray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int globalSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            globalSum = Math.max(currentSum, globalSum);
+        }
+        return globalSum;
+    }
+}
